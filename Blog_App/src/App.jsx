634fc96a34dc1a4d7f3368b0753b5aad175b023f
Vaibhav <chaudharyvaibhav454@gmail.com>
2024-03-05 09:26:56 +0530

    first commit

---
+++ Blog_App/src/App.jsx
@@ -0,0 +1,42 @@
+import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import "./App.css";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+
+import  Footer from './component/Footer/Footer.jsx'
+import  Header from './component/Header/Header.jsx'
+
+import { Outlet } from 'react-router-dom'
+
+
+function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, )
+  
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        TODO:  <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
+}
+
+export default App
---
+++ Blog_App/src/component/container/Container.jsx
@@ -0,0 +1,7 @@
+//container is like a box
+function Container({children}) {
+    return <div className='w-full max-w-7xl mx-auto px-4'>{children}</div>;
+    
+  }
+  
+  export default Container
